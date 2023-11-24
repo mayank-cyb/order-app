a66b09f38d96b01185bc1ba003141a65f7544539
mayank-cyb <73458158+mayank-cyb@users.noreply.github.com>
2023-11-24 09:33:10 +0530

    Add files via upload

---
+++ DeviceTable.vue
@@ -0,0 +1,37 @@
+<template>
+  <table class="table table-bordered" aria-describedby="" cellpadding="0" cellspacing="0">
+    <tr>
+        <th v-for="n in 6" :key="`th-${n}`">{{ n }}</th>
+    </tr>
+    <tr v-for="n in 7" :key="`tr-${n}`">
+      <td v-for="n in 6" :key="`td-${n}`">Print Status {{ n }}</td>
+    </tr>
+  </table>
+</template>
+
+<style lang="scss">
+    @import '../assets/variables.scss';
+    table {
+        display: grid;
+        //max-width: 50.4375rem;
+        margin: 0 0 0 auto; 
+    }
+    tr {
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        height: 5rem;
+        &:nth-of-type(2n+1) {
+            background: $colorGrey;
+        }
+    }
+    th,
+    td {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-right: 0.125rem solid #D2D8E3;
+        &:last-of-type {
+            border-right: none !important;
+        }
+    }
+</style>
---
+++ OrdersCompleted.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>Area for completed orders view</h2>
+</template>
+
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
