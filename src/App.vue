a4736c12757d23fb014efe751162ee6e07e21233
mayank-cyb <73458158+mayank-cyb@users.noreply.github.com>
2023-12-22 12:12:21 +0530

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
-  <v-app>
-    <the-header />
-    <navigation-left @drawer-link-clicked="toggleDrawerStatus" @other-links-clicked="closeDrawer" :drawer="drawer"/>
-    <navigation-drawer-left v-model="drawer" @click="toggleDrawerStatus"/>
+  <VApp>
+    <section class="header-section">
+      <LsxHeader />
+    </section>
+    <NavigationLeft
+      :drawer="drawer"
+      @drawer-toggle-link-clicked="toggleDrawerStatus"
+      @router-link-clicked="closeDrawer"
+    />
+    <NavigationDrawerLeft
+      v-model="drawer"
+      @click="toggleDrawerStatus"
+    />
     <v-main class="main-content">
       <section class="main-content__dynamic-view">
         <v-card>
@@ -13,17 +22,15 @@
         <router-view name="WaitingOrders"></router-view>
       </section>
     </v-main>
-  </v-app>
+  </VApp>
 </template>
 
 <script setup>
-import TheHeader from './components/TheHeader.vue'
-import NavigationLeft from './components/NavigationLeft.vue'
-// eslint-disable-next-line no-unused-vars
-import NavigationDrawerLeft from './components/NavigationDrawerLeft.vue'
-import { ref } from 'vue'
-import '@/assets/global.scss'
-
+    import LsxHeader from './components/LsxHeader.vue'
+    import NavigationLeft from './components/NavigationLeft.vue'
+    import NavigationDrawerLeft from './components/NavigationDrawerLeft.vue'
+    import { ref } from 'vue'
+    import '@/assets/global.scss'
     let drawer = ref(false)
     const toggleDrawerStatus = () => {
       drawer.value = !drawer.value
@@ -35,42 +42,60 @@ import '@/assets/global.scss'
 
 <style lang="scss" scoped>
 @import './src/assets/variables.scss';
-
 .v-application {
-  align-self: center;
-  background: $colorRpxBg !important;
-  font-display: $fontRoboto;
-  // max-width: 120rem;
-  // width: 100%;
-  //margin: 0 auto;
+  background: $color_rpx_bg ;
+  font-display: $font_roboto;
+  min-height: 65.5rem !important;
 }
 .v-app-bar {
-  background: $colorRpxBg !important;
+  background: $color_rpx_bg;
 }
-.v-main {
+.header-section{
+  z-index: 1003;
+}
+:deep(.v-main) {
   position: relative;
-  background: $colorRpxBg;
-  padding: 5rem 2.375rem 5rem 6.25rem;
-  min-height: 109.25rem !important; 
-}
-.main-content,
-.bottom-content {
-  border-radius: 0.75rem;
-  overflow: hidden;
+  background: $color_rpx_bg;
+  padding: 5rem 2.375rem 14.5rem 6.25rem;
 }
 .main-content {
   display: flex;
-  gap: 1rem !important;
+  gap: 1rem;
   flex-direction: column;
-  &__dynamic-view {
-    background: $colorWhite !important;
+  &__dynamic-view,
+  &__headline,
+  &__waiting-orders {
+    background: $color_white;
     border-radius: 0.75rem;
-    height: 51.25rem !important;
+  }
+  &__dynamic-view {
+    height: 51.25rem;
     padding: 1.125rem;
+  }
+  &__headline {
+    background: $color_rpx_bg;
+    padding-left: 2.0625rem;
+    .v-card {
+      background: $color_rpx_bg;
+      color: $color_white;
+      box-shadow: none;
+      &-title {
+        letter-spacing: normal;
+        font-size: 1.875rem;
+        line-height: 2.25rem;
+        font-weight: $black;
+        border-style: none;
+        padding: 0;
+      }
+    }
+  }
+  &__waiting-orders {
+    height: 31.875rem;
+    background: $color_white;
   }
 }
 .v-card {
   border-style: none;
-  box-shadow: none !important;
+  box-shadow: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,23 +1,22 @@
-import './assets/main.css'
 import { createI18n } from 'vue-i18n'
 import messages from './localizations/localizations'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import '@fssd/rpx-app-icons';
+import '@fssd/rpx-app-icons'
 
-;(async () => {
-  const app = createApp(App)
-  const uiLanguage = 'en' //TODO:Get Value from config  await lsxConfig.get('uiLanguage')
-  const i18n = createI18n({
-    locale: uiLanguage, // set locale
-    fallbackLocale: 'en', // set fallback locale
-    messages: messages.messages // set locale messages
-  })
-  app.use(i18n)
-  app.use(router)
-  app.use(vuetify)
-  window.i18n = i18n
-  app.mount('#app')
-})()
+const app = createApp(App)
+const uiLanguage = 'en' //TODO:Get Value from config  await lsxConfig.get('uiLanguage')
+const i18n = createI18n({
+  locale: uiLanguage, // set locale
+  legacy: false,
+  allowComposition: true,
+  fallbackLocale: 'en', // set fallback locale
+  messages: messages.messages // set locale messages
+})
+app.use(i18n)
+app.use(router)
+app.use(vuetify)
+window.i18n = i18n
+app.mount('#app')
