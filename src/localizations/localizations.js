dcc9f99639b64cf9b4a1eca5b5b81fdda6a25d67
mayank-cyb <73458158+mayank-cyb@users.noreply.github.com>
2023-12-22 12:13:33 +0530

    Add files via upload

--- src/localizations/localizations.js
+++ src/localizations/localizations.js
@@ -2,7 +2,32 @@ const messages = {
   en: {
     common: {
       back: 'Back'
-    }
+    },
+    btn: {
+      orders: 'Orders',
+      kiosks: 'Kiosks',
+      systems: 'Systems',
+      links: 'Links',
+      passport: 'Passport',
+      help: 'Help',
+      closeLab: 'Close Lab',
+      port: 'Port',
+      oneWalmart: 'OneWalmart',
+      servicePortal: 'Service Portal',
+      yesVideo: 'Yes Video',
+      orderStatus: 'Order Status',
+      marketing: 'Marketing',
+    },
+    headline: {
+      links: 'Links',
+      orders: 'Orders',
+      waiting_orders: 'Waiting Orders'
+  },
+    links: {
+      active: 'Active',
+      completed: 'Completed'
+    },
+    appVersion: 'V 1100.0.0'
   },
   es: {
     common: {
